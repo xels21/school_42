8427e2c7fdae6ee89c07c2434044b5fa5fb65fe5
xels <dean.poslovsky@gmail.com>
2025-09-12 14:34:21 +0200

    added: ft_putstr

---
+++ exam-rank-02/level1/ft_putstr/build.sh
@@ -0,0 +1,2 @@
+gcc -Wall -o ft_putstr ft_putstr.c
+./ft_putstr "print this message"
---
+++ exam-rank-02/level1/ft_putstr/ft_putstr.c
@@ -0,0 +1,25 @@
+/*
+Write a function that displays a string on the standard output.
+
+The pointer passed to the function contains the address of the string's first
+character.
+
+Your function must be declared as follows:
+
+void	ft_putstr(char *str);
+*/
+#include <unistd.h> ///< for write
+void ft_putstr(char *str)
+{
+  while (*str)
+  {
+    write(1, str, 1);
+    str++;
+  }
+}
+
+int main(int argc, char const *argv[])
+{
+  ft_putstr((char *)argv[1]);
+  return 0;
+}
